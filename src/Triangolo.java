9d114215b4ea598185982e3b1abbf99a71953ac8
SIlvioVutea <silviovutea@gmail.com>
2024-02-23 13:15:40 +0100

    Initial commit

---
+++ src/Triangolo.java
@@ -0,0 +1,32 @@
+public class Triangolo implements Forma {
+    private double base = 0.0;
+    private double altezza = 0.0;
+
+
+    public Triangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return (base * altezza) / 2;
+    }
+
+}
